80a1f543ce65d466ce4810a8104cbf8f4f16810a
清妍 <@>
2020-07-22 09:47:14 +0800

    123

---
+++ IOT_Project_WebAdmin/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IOT_Project_WebAdmin.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+    }
+}
